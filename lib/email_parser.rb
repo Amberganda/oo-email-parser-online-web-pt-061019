3e955b38e586f15e9429e4c24b6c0b827457e79a
Amber Taranto <ambzzzz@gmail.com>
2019-07-31 22:22:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,10 +3,10 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailParser
+class EmailAddressParser
     attr_accessor :email_addresses
 
-    
+
 
 
 end
